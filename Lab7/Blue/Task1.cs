ffe466b8bfa0dd1ee1cf5d988954933bde9cd45c
WoblaSM <118443431+WoblaSM@users.noreply.github.com>
2026-03-08 14:54:48 +0300

    Task1
    Added Response struct with properties and methods for vote counting and printing.

--- Lab7/Blue/Task1.cs
+++ Lab7/Blue/Task1.cs
@@ -1,6 +1,53 @@
-﻿namespace Lab7.Blue
+using System.ComponentModel;
+
+namespace Lab7.Blue
 {
     public class Task1
     {
+        public struct Response
+        {
+            private string _name;
+            private string _surname;
+            private int _votes;
+            public string Name => _name;
+            public string Surname => _surname;
+            public int Votes => _votes;
+
+
+
+            public Response(string name, string surname)
+            {
+                this._name = name;
+                this._surname = surname;
+
+            }
+            public int CountVotes(Response[] responses)
+            {
+                int local_votes = 0;
+                foreach (Response response in responses)
+                {
+                    if (Name == response.Name & Surname == response.Surname)
+                    {
+                        local_votes++;
+                    }
+                }
+
+                for (int i = 0; i < responses.Length; i++)
+                {
+                    if (Name == responses[i].Name & Surname == responses[i].Surname)
+                    {
+                        responses[i]._votes = local_votes;
+                    }
+                }
+
+                return Votes;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Name: {Name}\n Surname: {Surname}\n Votes: {Votes}\n");
+            }
+
+        }
     }
 }
+
